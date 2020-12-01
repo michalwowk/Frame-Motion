4ff3d29e77670f8cc5c55c79bea9af644147a6ed
Michal Wowk <michal.wowk@chop-chop.org>
2020-12-01 21:34:43 +0100

    Use variants to make code more clear and ad orchestrated animation

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,34 +2,62 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: "0.5",
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw",
+  },
+  visible: {
+    x: 0,
+    transition: { type: "spring", stiffness: 120 },
+  },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 0.5 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
         {bases.map((base) => {
           let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
+            <motion.li
+              whileHover={{ scale: 1.3, originX: 0, color: "#f8e112" }}
+              transition={{ type: "spring", stiffness: 300 }}
+              key={base}
+              onClick={() => addBase(base)}
+            >
               <span className={spanClass}>{base}</span>
-            </li>
+            </motion.li>
           );
         })}
       </ul>
 
       {pizza.base && (
         <motion.div
-          animate={{ x: 0 }}
-          initial={{ x: "-100vw" }}
-          transition={{ type: "spring", stiffness: 120 }}
           className="next"
+          variants={nextVariants}
         >
           <Link to="/toppings">
             <motion.button
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -12,8 +12,28 @@ const Toppings = ({ addTopping, pizza }) => {
     "tomatoes",
   ];
 
+  const containerVariants = {
+    hidden: {
+      opacity: 0,
+      x: "100vw",
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        delay: "0.5",
+      },
+    },
+  };
+
   return (
-    <div className="toppings container">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="toppings container"
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map((topping) => {
@@ -42,7 +62,7 @@ const Toppings = ({ addTopping, pizza }) => {
           Order
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
